c58c9024106b8bd27011ece3bdcec4e8d0f9ed41
ShibaeO <antoine@quetel.pro>
2020-07-22 19:02:27 +0200

    - ajout info durant le process (temp restant,video restant, total)

--- final.py
+++ final.py
@@ -6,11 +6,26 @@ clipList = []
 
 a = os.listdir("outpout/")
 
-for i in a:
-    if ".mp4" in i:
-        print(i)
-        clipList.append(VideoFileClip(f"outpout/{i}"))
+countr = 0
+for i in range(len(a)):
+    start_time = time.time()
+    clipList.append(VideoFileClip(f"outpout/{i}.mp4"))
 
-print(clipList)
+    end_time = time.time()
+    execTime = end_time - start_time
+    left = len(a) - countr
+    aa = time.strftime("%H:%M:%S", time.gmtime(int(float(execTime) * float(left))))
+    os.system("cls")
+    print(f"[+]    Finish : {i} | Left : {left} | Estimate time : {aa}")
+    countr += 1
+
 final = concatenate_videoclips(clipList)
 final.write_videofile("final/final.mp4", fps=30, codec="libx264")
+
+os.system("cls")
+print("[+]    Clean Up !!")
+
+for i in range(len(a)):
+    os.remove(f"outpout/{i}.mp4")
+    os.remove(f"content/{i}.mp3")
+    os.remove(f"content/{i}.png")
